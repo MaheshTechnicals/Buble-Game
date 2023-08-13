bb59365919f0150a3a0cbf982569fcc3fac50851
msvarma10001@gmail.com <msvarma10001@gmail.com>
2023-08-13 14:04:26 +0530

    Rady game

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 let time = 60;
 let score = 0;
-
+let newhit = 0;
 let mkbuble = () => {
 
   for (var i = 0; i < 30; i++) {
     let rn = Math.floor(Math.random() * 10)
+    newhit = rn
     let a = `<div class="bubule">${rn}</div>`
     document.querySelector("#bublebox").innerHTML += a
   }
@@ -14,7 +15,7 @@ let hitgen = () => {
 
   let hitnumber = Math.floor(Math.random() * 10)
 
-  document.querySelector("#hit").textContent = hitnumber
+  document.querySelector("#hit").textContent = newhit
 
 }
 
@@ -37,12 +38,27 @@ let allbtn = () => {
   let button = document.querySelector("#bublebox")
   button.addEventListener("click", (e) => {
 
-    console.log(Number(e.target.textContent))
+    let hit = document.querySelector("#hit").textContent
+
+
+    if (Number(hit) == Number(e.target.textContent)) {
+      score += 10
+      let scorecount = document.querySelector("#score").textContent = score
+      document.querySelector("#bublebox").innerHTML = ""
+      mkbuble()
+      hitgen()
+    }
+    else {
+      score -= 10
+      let scorecount = document.querySelector("#score").textContent = score
+      document.querySelector("#bublebox").innerHTML = ""
+      mkbuble()
+      hitgen()
+    }
+
+
 
   })
-
-
-
 
 
 }
@@ -52,7 +68,7 @@ let allbtn = () => {
 
 
 
-hitgen()
 mkbuble()
+hitgen()
 timer()
 allbtn()
