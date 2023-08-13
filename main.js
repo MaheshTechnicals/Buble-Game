528e04cceb00d3778e1104d5cd3619308212119c
msvarma10001@gmail.com <msvarma10001@gmail.com>
2023-08-13 12:58:33 +0530

    Hit and timer on

---
+++ main.js
@@ -0,0 +1,45 @@
+let time = 60;
+let score = 0;
+
+let mkbuble = () => {
+
+  for (var i = 0; i < 30; i++) {
+    let rn = Math.floor(Math.random() * 10)
+    let a = `<div class="bubule">${rn}</div>`
+    document.querySelector("#bublebox").innerHTML += a
+  }
+}
+
+let hitgen = () => {
+
+  let hitnumber = Math.floor(Math.random() * 10)
+
+  document.querySelector("#hit").textContent = hitnumber
+
+}
+
+
+let timer = () => {
+  let setTime = setInterval(() => {
+    time--
+
+    if (time == 0) {
+      clearInterval(setTime)
+      document.querySelector("#bublebox").innerHTML = `<h2>Your Score = ${score}`
+    }
+    document.querySelector("#timer").textContent = time
+
+  }, 1000)
+
+}
+
+
+
+
+
+
+
+
+hitgen()
+mkbuble()
+timer()
